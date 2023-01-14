eec617f140ba00ba75479d45d36e9faff169fbe6
Matthew-828 <78410812+Matthew-828@users.noreply.github.com>
2023-01-15 01:29:15 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 #include<fstream>
 #include<iomanip>
-
+#include <ctype.h> // to check isdigit
 using namespace std;
 
+void login_menu();
+void login();
+void signUp();
 
-
-
-
+/*
 Dentise - Own username and password
 -edit personal details such as their name, age, gender, and phone number
 -submit available timeslots patient appointments booking.
@@ -70,20 +71,94 @@ ADD USERS
 3
 ----------------------------------------
 Perform task acordingly
-
-
-
-
-
-
-
-
+*/
 
 
 int main()
 {
+    login_menu();
 
 
+
+
+    return 0;
+}
+
+
+
+void login_menu ()
+{
+    int login_menu_choice = 0;
+
+	cout << "|===================================|\n";
+    cout << "|             Login/SignUp          |\n";
+    cout << "|           ----------------        |\n";
+    cout << "|    1. Login                       |\n";
+    cout << "|    2. Sign Up                     |\n";
+    cout << "|    3. Exit                        |\n";
+	cout << "|===================================|\n";
+
+    cout << "Welcome, ";
+
+re_prompt1:
+    cout << "Please select 1-3 :";
+re_prompt2:
+    cin >> login_menu_choice;
+
+    switch(login_menu_choice) {
+            case 1:{
+                login();    //Go to login function
+            }
+            break;
+
+            case 2:
+            {
+                signUp();    //Go to Sign up function
+            }
+            break;
+
+            case 3:
+            {
+                cout << "Exiting program ....\n\n";
+                exit(0);
+            }
+            break;
+
+            default:
+            {
+                cout << "Please select only from 1 to 3! : ";
+                goto re_prompt2;
+                break;
+            }
+
+        }
 
 
 }
+
+void login()
+{
+    //prompt user name
+    //prompt password
+    //check if exist
+    //dentist ? patient ?
+    //apply functions available
+    cout <<"Login function"; //Testing
+
+}
+
+void signUp()
+{
+    //prompt user name
+    //prompt password
+    //prompt confirm password
+    //check if user name exist = add / re_signUp
+    //Prompt details name, age, gender, ic number and phone number
+    //cout information added
+    //navigate to patient menu
+    cout <<"Sign Up function"; //Testing
+
+}
+
+
+
